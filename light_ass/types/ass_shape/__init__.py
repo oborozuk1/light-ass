dcb2e3a3c1c404276fc2f12ad5ea16a2fa53aa2f
Oborozuki <oborozuk1@qq.com>
2026-07-15 18:38:02 +0800

    feat: expose drawing command classes and point representation in __all__

--- light_ass/types/ass_shape/__init__.py
+++ light_ass/types/ass_shape/__init__.py
@@ -1,5 +1,25 @@
+from .command import (
+    BaseDrawCmd,
+    BezierCmd,
+    BSplineCmd,
+    CloseCmd,
+    ExtendSplineCmd,
+    LineCmd,
+    MoveCmd,
+    MoveNoClosingCmd,
+)
+from .point import Point
 from .shape import AssShape
 
 __all__ = [
     "AssShape",
+    "BaseDrawCmd",
+    "BezierCmd",
+    "BSplineCmd",
+    "CloseCmd",
+    "ExtendSplineCmd",
+    "LineCmd",
+    "MoveCmd",
+    "MoveNoClosingCmd",
+    "Point",
 ]
